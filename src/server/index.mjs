b69ba699117326e5ebd3b5033dc1a1b93f8801a5
Roma-Kyrnis <kirnos007@gmail.com>
2023-11-11 15:28:48 +0200

    fix: cors

--- src/server/index.mjs
+++ src/server/index.mjs
@@ -5,22 +5,33 @@ import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import morganBody from 'morgan-body';
 
 import config from '../config/index.mjs';
 import router from './router/index.mjs';
 
 const app = express();
 
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+morganBody(app);
 
 app.use(
   morgan(config.envs.NODE_ENV === config.constants.NODE_ENVS.DEVELOPMENT ? 'dev' : 'combined'),
 );
+app.use(morgan(':req[headers] :res[headers]'));
 app.use(helmet());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use('/api', router);
+app.use(morgan(':req[headers] :res[headers]'));
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
